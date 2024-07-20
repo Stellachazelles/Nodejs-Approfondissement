107c6a45869d47608d4fd8ba1fbb984a04a33011
stella <stella.chazelles@gmail.com>
2024-07-21 00:33:37 +0200

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "app",
-      script: "./www/app.js",
-      env_production: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+      name: 'myapp',
+      script: './server.js',
+      instances: 3, // Lancer 3 instances en parallèle
+      exec_mode: 'cluster',
+      max_memory_restart: '200M', // Utilisation maximale de la mémoire: 200 Mo
+      error_file: './logs/err.log', // Fichier log en cas d’erreur
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }
+  ]
 };
